1a3440a9c4c94aaf125a36ecaf0d11e4fbe2532e
Italo1994 <italohortiz2015@gmail.com>
2022-03-19 22:56:33 +0000

    desenvolvendo a função para criar evento de clique nos campos e colocar o respectivo marcador

--- js/script.js
+++ js/script.js
@@ -1,37 +1,40 @@
 const ids = [];
 var jogadaAtual=0; // verifica a jogada atual entre os jogadores
 
+function start() {
+    iniciaIds();
+}
+
 function iniciaIds() {
+    let target = false;
+    let index;
     
     for(let i=0; i<9; i++) {
         ids[i] = 'a' + (i+1);
     }
 
-    insereEvento();
-}
-
-function insereEvento() {
-    
     for(let i=0; i<9; i++) {
         ids[i] = document.getElementById(ids[i]);
-        
     }
 
     for(let i=0; i<9; i++) {
         ids[i].addEventListener('click', function(event) { // verifica o evento de clique de cada campo da matriz
+            index = i;
             preencheCampo(i); // envia o índice do elemento da matriz que efetuou o disparo do evento
+            console.log('cliquei no elemento de índice ' +index);
+            return;
         });
     }
+
 }
 
 function preencheCampo(index) {
 
     jogadaAtual++;
-    console.log(jogadaAtual);
+    console.log('jogada atual = ' +jogadaAtual);
 
     for(let i=0; i<9; i++) {
         if(index == i) {
-            //console.log(index)
             if(ids[index].innerHTML !== " " && jogadaAtual%2 !== 0 ){
                 ids[index].innerHTML = "X";
             } else {
@@ -42,11 +45,3 @@ function preencheCampo(index) {
     }
 
 }
-
-function start() {
-    iniciaIds();
-}
-
-setTimeout(() => {
-     start();   
-}, 1000);
