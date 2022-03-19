b3012543375f74acba439a19d4d2eec869a99d9c
Italo1994 <italohortiz2015@gmail.com>
2022-03-19 23:41:16 +0000

    alterando função preencheCampo

--- js/script.js
+++ js/script.js
@@ -35,24 +35,16 @@ function preencheCampo(index) {
 
     for(let i=0; i<9; i++) {
         if(index == i) {
-            if(ids[index].innerHTML.length === 0) {
+            if(ids[index].innerHTML !== "X" && ids[index].innerHTML !== "O") {
                 if( jogadaAtual%2 !== 0 ){
                     console.log('estava vazio')
                     ids[index].innerHTML = "X";
                 }
-                
-            } 
-            
-            if(ids[index].innerHTML.length === 0) {
                 if( jogadaAtual%2 === 0 ){
                     console.log('estava vazio')
                     ids[index].innerHTML = "O";
                 }
-                
-            }
-            
-            
-            
+            }       
         }
     }
 
